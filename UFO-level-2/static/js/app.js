3821e82f05bf5cf03232efb17c5431c3c9d246fa
bakshishreya <bakshishreya@gmail.com>
2021-03-15 08:08:30 -0400

    Adding level 2

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,4 +1,44 @@
 // from data.js
 var tableData = data;
+var tbody = d3.select('tbody');
+var input = d3.selectAll('input');
+var btn = d3.select('button');
 
+populateTable(tableData);
+input.on('change', handleChange);
+btn.on('click', handleClick);
 // YOUR CODE HERE!
+function populateTable(data) {
+    tbody.html("");
+
+    data.forEach(dataRow => {
+        var row = tbody.append('tr');
+
+        Object.values(dataRow).forEach(val => {
+            var cell = row.append('td');
+
+            cell.text(val);
+        });
+    });
+};
+
+var filteredData = tableData;
+
+function handleChange() {
+
+    var key = d3.select(this).property('id');
+    var value = d3.select(this).property('value');
+
+    if (value) {
+        filteredData = filteredData.filter(row => row[key] === value);
+    };
+
+    populateTable(filteredData);
+}
+
+function handleClick() {
+    input.property("value", "");
+    filteredData = tableData;
+
+    populateTable(filteredData);
+}
